af85a37f91799ee86440d8a058a777ae15e54612
dmullier <d.mullier@leedsbeckett.ac.uk>
2023-02-16 16:51:18 +0000

    Add files via upload
    Initial commit. Drag and drop files from local project.

---
+++ SpringSOAP-server/src/main/java/com/example/demo/SpringSoapServerApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringSoapServerApplication {
+
+	public static void main2(String[] args) {
+		SpringApplication.run(SpringSoapServerApplication.class, args);
+	}
+
+}
